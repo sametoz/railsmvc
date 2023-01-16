5ca6c03d3fc2a70338db0a77b4c74dd884db719c
Samet Öz <samet.oz17@gmail.com>
2023-01-16 16:02:24 +0300

    fix route endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-
   # Bir model için otomatik olarak oluşturulan CRUD route'ları için:
   resources :articles
 end
