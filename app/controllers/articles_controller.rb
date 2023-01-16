be69eba1ecf8a73c83c5848c1edc7fb883ff2f98
Samet Öz <samet.oz17@gmail.com>
2023-01-16 23:20:46 +0300

    add crud operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,30 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit 
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    if @article.destroy
+      redirect_to root_path, status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :body)
