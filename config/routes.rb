5a5f27119c70e516a08f40155329e403095af9c4
Samet Öz <samet.oz17@gmail.com>
2023-01-16 15:53:42 +0300

    add methods to be articles as a resource use article_path extension for clean code

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "articles#index"
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
+
+  # Bir model için otomatik olarak oluşturulan CRUD route'ları için:
+  resources :articles
 end
