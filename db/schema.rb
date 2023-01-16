38efc4eccf15d4cfbb8f7d27fce8035a7157c065
Samet Öz <samet.oz17@gmail.com>
2023-01-16 15:18:25 +0300

    add some erb code and show view

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2023_01_16_114323) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
